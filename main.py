17758e4c202aefd55362df92a06d67f8a65c81f5
cam <cameronmilsom7@gmail.com>
2025-10-02 15:29:06 +0100

    Categorizing transactions

--- main.py
+++ main.py
@@ -13,13 +13,29 @@ if "categories" not in st.session_state:
         "Uncategorized": []
     }
 
-if os.path.exists("category_file"):
-    with open("category_file", "r") as f:
+if os.path.exists(category_file):
+    with open(category_file, "r") as f:
         st.session_state.categories = json.load(f)
 
 def save_categories():
-    with open("category_file", "w") as f:
+    with open(category_file, "w") as f:
         json.dump(st.session_state.categories, f)
+
+def categorize_transactions(df):
+    df["Category"] = "Uncategorized"
+
+    for category, keywords in st.session_state.categories.items():
+        if category == "Uncategorized" or not keywords:
+            continue
+
+        lowered_keywords = [keyword.lower().strip() for keyword in keywords]
+
+        for idx, row in df.iterrows():
+            details = row["Details"].lower().strip()
+            if details in lowered_keywords:
+                df.at[idx, "Category"] = category
+
+    return df
 
 def load_transactions(file):
     try:
@@ -28,11 +44,19 @@ def load_transactions(file):
         df["Amount"] = df["Amount"].str.replace(",", "").astype(float)
         df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y")
 
-        
-        return df
+        return categorize_transactions(df)
     except Exception as e:
         st.error(f"Error processing file: {str(e)}")
         return None
+    
+def add_keyword_to_category(category, keyword):
+    keyword = keyword.strip()
+    if keyword and keyword not in st.session_state.categories[category]:
+        st.session_state.categories[category].append(keyword)
+        save_categories()
+        return True
+    
+    return False
 
 def main():
     st.title("Simple Finance Dashboard")
@@ -55,7 +79,6 @@ def main():
                 if new_category not in st.session_state.categories:
                     st.session_state.categories[new_category] = []
                     save_categories()
-                    st.success(f"Added a new category: {new_category}")
                     st.rerun()
 
 
